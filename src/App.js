53e4ae38b0918f94d43dbc2baefd3ca4a964c246
apollo <apollo@DESKTOP-UPD06S5>
2022-03-04 12:54:08 -0400

    Cleaning up app.js and removing its css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
 import LandingPage from './components/LandingPage';
 
 function App() {
   return (
     // You can only return a single parent element (i.e. only the div)
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          &#123; Hello, my name is Lorem.<br></br>
-          I'm a back-end developer &#125;
-        </p>
-      </header>
+    <div className="container">
       <LandingPage />
     </div>
   );
