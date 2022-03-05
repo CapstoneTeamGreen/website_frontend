960cc1510f68753d91ef13d041454a7ae8286f77
apollo <apollo@DESKTOP-UPD06S5>
2022-03-05 17:26:52 -0400

    Fighting with tsparticles and swapped to styled-components

--- src/components/LandingPage/LandingElements.js
+++ src/components/LandingPage/LandingElements.js
@@ -12,18 +12,6 @@ export const LandingContainer = styled.div`
     z-index: 1;
 `
 
-export const LandingBg = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-    z-index: 2;
-`
-
 export const LandingContent = styled.div`
     z-index: 3;
     max-width: 1200px;
--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,31 +1,27 @@
 import React from 'react'
-// import ParticleBackground from './ParticleBackground'
 import Particles from 'react-tsparticles'
-import ParticleConfig from './config/ParticleConfig'
+import ParticleConfig from './ParticleConfig'
 import {
     LandingContainer,
-    LandingBg,
     LandingContent,
     LandingP,
     LandingSpan    
 } from './LandingElements'
 
 const LandingPage = () => {
-  return (
-      <LandingContainer>
+    return (
+        <LandingContainer>
            
-        <LandingBg>
-            <Particles params={ParticleConfig} />
-        </LandingBg>
-
-        <LandingContent>
-            <LandingP>Hello, my name is
-            <LandingSpan> Lorem</LandingSpan><br></br>
-            I'm a 
-            <LandingSpan> back-end </LandingSpan>
-            developer
-            </LandingP>
-        </LandingContent>
+            {/* <Particles container={LandingContainer} params={ParticleConfig} /> */}
+            
+            <LandingContent>
+                <LandingP>Hello, my name is
+                <LandingSpan> Lorem</LandingSpan><br></br>
+                I'm a 
+                <LandingSpan> back-end </LandingSpan>
+                developer
+                </LandingP>
+            </LandingContent>
 
       </LandingContainer>
     )
