f130a7eb4ebd9bc9a6bb3a8e12e4b9c6f5498a4f
Ryan <w0201814@nscc.ca>
2022-03-05 12:47:34 -0400

    implemented button funct, can not click.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,18 +5,24 @@ import cube from "../../../src/images/cube.png"
 const About = ({ title, blurb, image }) => {
   const technologies = ["C++", "mySQL", "Laravel", "Java", "HTML", "JavaScript", "ReactJS", "C#", "PHP"];
   const certifications = ["Microsoft Certified Solutions Developer", "CompTIA Security+", "Google Certified Professional Cloud Architect"];
+  let skillsSelected = true;
 
+  const toggled = () => {
+    skillsSelected = !skillsSelected;
+    console.log("check!");
+  }
 
   return (
     <div className="container">
-      <div class="grid">
-      <div class="grid-header">{title}</div>
-      <div class="grid-blurb">{blurb}</div>
-      <div class="grid-image">
-        <img src={image} alt="grid image"/>
-      </div>
-      <div class="grid-view-toggle">
-        show certifications
+      <div className="grid">
+        <div className="grid-header">{title}</div>
+        <div className="grid-blurb">{blurb}</div>
+        <div className="grid-image">
+          <img src={image} alt="grid image"/>
+        </div>
+        <div className="grid-view-toggle">
+          <button className="btn-toggle" onClick={toggled}>
+          {skillsSelected ? 'show certifications' : 'show technologies'}</button>
       </div>
     </div>
   </div>
