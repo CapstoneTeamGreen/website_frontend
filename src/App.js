4c2c5178e47ab28006ecffb151a117e13a28486c
apollo <apollo@DESKTOP-UPD06S5>
2022-03-05 10:28:44 -0400

    particlejs basic functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import LandingPage from './components/LandingPage';
+import ParticleBackground from './components/LandingPage/ParticleBackground';
 
 function App() {
   return (
     // You can only return a single parent element (i.e. only the div)
     <div className="container">
+      <ParticleBackground />
       <LandingPage />
     </div>
   );
---
+++ src/components/LandingPage/ParticleBackground.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Particles from 'react-tsparticles'
+import ParticleConfig from './config/ParticleConfig'
+
+const ParticleBackground = () => {
+  return (
+    <Particles params={ParticleConfig} />
+  )
+}
+
+export default ParticleBackground
