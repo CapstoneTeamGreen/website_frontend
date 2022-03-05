e07bb294e3f0c4ea7b1f572ca831dc74da185c5d
Ryan <w0201814@nscc.ca>
2022-03-05 11:38:35 -0400

    about component half way done, formatting needed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import LandingPage from './components/LandingPage';
+import About from './components/About';
 
 function App() {
   return (
     // You can only return a single parent element (i.e. only the div)
     <div className="container">
       <LandingPage />
+      <About />
     </div>
   );
 }
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,11 +1,32 @@
 import React from 'react'
+import './About.css'
+import cube from "../../../src/images/cube.png"
 
-const About = () => {
+const About = ({ title, blurb, image }) => {
+  const technologies = ["C++", "mySQL", "Laravel", "Java", "HTML", "JavaScript", "ReactJS", "C#", "PHP"];
+  const certifications = ["Microsoft Certified Solutions Developer", "CompTIA Security+", "Google Certified Professional Cloud Architect"];
+
+
   return (
-    <div className='container'>
-      
+    <div className="container">
+      <div class="grid">
+      <div class="grid-header">{title}</div>
+      <div class="grid-blurb">{blurb}</div>
+      <div class="grid-image">
+        <img src={image} alt="grid image"/>
+      </div>
+      <div class="grid-view-toggle">
+        show certifications
+      </div>
     </div>
+  </div>
   )
+}
+
+About.defaultProps = {
+  title: "Who Am I",
+  blurb: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac felis donec et odio pellentesque diam. Platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper.",
+  image: cube
 }
 
 export default About
