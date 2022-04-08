f3990f237319994e5e3ace868a5406c18968e534
apollo <apollo@DESKTOP-UPD06S5>
2022-04-08 12:26:17 -0300

    Default objects created in the event the api fails a connection

---
+++ src/DefaultProjects.js
@@ -0,0 +1,34 @@
+export const project1 = {
+  "projectId": 1,
+  "projectName": "Project 1",
+  "description": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto.",
+  "projectLink": "https://nscc.ca"
+}
+
+export const project2 = {
+  "projectId": 2,
+  "projectName": "Project 2",
+  "description": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto.",
+  "projectLink": "https://nscc.ca"
+}
+
+export const project3 = {
+  "projectId": 3,
+  "projectName": "Project 3",
+  "description": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto.",
+  "projectLink": "https://nscc.ca"
+}
+
+export const project4 = {
+  "projectId": 4,
+  "projectName": "Project 4",
+  "description": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto.",
+  "projectLink": "https://nscc.ca"
+}
+
+export const project5 = {
+  "projectId": 5,
+  "projectName": "Project 5",
+  "description": "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto.",
+  "projectLink": "https://nscc.ca"
+}
